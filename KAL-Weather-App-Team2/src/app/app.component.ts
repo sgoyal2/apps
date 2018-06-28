085b99dc4857f72fc69dbe9d3074aab5185e913b
aliceluo2048 <37196330+aliceluo2048@users.noreply.github.com>
2018-06-28 10:33:09 -0700

    Search form and component

--- KAL-Weather-App-Team2/src/app/app.component.ts
+++ KAL-Weather-App-Team2/src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div style="text-align:center">
+    <h1>Foo Weather App</h1>
+    <div>Search for your city</div>
+    <div>
+    <app-city-search></app-city-search></div>
+    <h2>Current Weather</h2>
+    <app-current-weather></app-current-weather>  
+  </div>  
+  `,
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
--- KAL-Weather-App-Team2/src/app/app.module.ts
+++ KAL-Weather-App-Team2/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { WeatherServiceService } from './weather-service/weather-service.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CitySearchComponent } from './city-search/city-search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    CitySearchComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [WeatherServiceService],
   bootstrap: [AppComponent]
