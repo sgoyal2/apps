0461ff8e547e2e54f74f6800e7133391a9888c28
PoojaPoluru <pooja.poluru95@gmail.com>
2018-06-29 11:33:43 -0700

    Angular Material

--- KAL-Weather-App-Team2/src/app/app.module.ts
+++ KAL-Weather-App-Team2/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import {  BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { WeatherService } from './weather/weather.service';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from './/material.module';
+import {HttpClientModule} from '@angular/common/http';
+//import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule,MatToolbarModule, MatButtonModule, MatIconModule,MatFormFieldModule} from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CitySearchComponent } from './city-search/city-search.component';
 
@@ -18,10 +20,19 @@ import { CitySearchComponent } from './city-search/city-search.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    MaterialModule,
+    MatFormFieldModule,
+    //MaterialModule,
     FormsModule,
     ReactiveFormsModule,
+    //FlexLayoutModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+
   ],
+  exports:[MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule,MatFormFieldModule,BrowserAnimationsModule],
   providers: [WeatherService],
   bootstrap: [AppComponent]
 })
--- KAL-Weather-App-Team2/src/app/current-weather/current-weather.component.ts
+++ KAL-Weather-App-Team2/src/app/current-weather/current-weather.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { ICurrentWeather } from '../icurrent-weather';
 import { WeatherService } from '../weather/weather.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  styleUrls: ['./current-weather.component.css'],
+  animations: [
+    // DEFAULT STATES
+    trigger('enterLeave', [
+      // :ENTER TRANSITION
+      // Transition Styles
+      transition('void => *', [
+        // 'From' styles
+        style({
+          opacity: 0.2,
+          transform: 'translateX(-100vw)'
+        }),
+        animate('1000ms ease-in',
+          // 'To' styles
+          // 1 - Comment this to remove the item's grow...
+          style({
+            opacity: 1,
+            transform: 'scale(1.2)'
+          })
+        )
+      ]),
+    ]), 
+  ]
 })
 export class CurrentWeatherComponent implements OnInit {
   current: ICurrentWeather
