efff7dd9f75da26239fb6883813740a0d6a237ca
sgoyal2 <35951861+sgoyal2@users.noreply.github.com>
2018-08-27 11:21:31 -0700

    Login and logout functionality

--- instagram-app-team2/client/src/App.js
+++ instagram-app-team2/client/src/App.js
@@ -2,12 +2,35 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authAction';
 import Register from "./components/auth/Register";
 import Footer from "./components/layout/Footer";
 import Login from "./components/auth/Login";
 
 import "./App.css";
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    //store.dispatch(clearCurrentProfile());
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
--- instagram-app-team2/client/src/components/auth/Login.js
+++ instagram-app-team2/client/src/components/auth/Login.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authAction';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Login extends Component {
   constructor() {
@@ -12,19 +16,41 @@ class Login extends Component {
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
   }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/feed');
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/feed');
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+
+    
   onSubmit(e) {
     e.preventDefault();
-    const user = {
+    const currentUser  = {
       email: this.state.email,
       password: this.state.password
     };
-    console.log(user);
+    // this will tigger the login user in authAction
+    this.props.loginUser(currentUser);
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="login">
         <div className="container">
@@ -35,24 +61,22 @@ class Login extends Component {
                 Sign in to your Instafake account
               </p>
               <form onSubmit={this.onSubmit}>
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className="form-control"
-                    name="email"
+                <TextFieldGroup
                     placeholder="Email Address"
+                    name="email"
+                    type="email"
                     value={this.state.email}
-                    onChange={this.onChange.bind(this)}
+                    onChange={this.onChange}
+                    error={errors.email}
                   />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className="form-control"
-                    name="password"
+
+                  <TextFieldGroup
                     placeholder="Password"
+                    name="password"
+                    type="password"
                     value={this.state.password}
-                    onChange={this.onChange.bind(this)}
+                    onChange={this.onChange}
+                    error={errors.password}
                   />
                   <input
                     type="submit"
@@ -61,7 +85,7 @@ class Login extends Component {
                   <p className="text-center">
                     Don't have an account? <Link to="/">Sign up</Link>
                   </p>
-                </div>
+                
               </form>
             </div>
           </div>
@@ -71,4 +95,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
